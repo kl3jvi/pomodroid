129ea2f807ae426837e8c7156ac382450dec75e8
kl3jvi <klejvisiper@gmail.com>
2021-08-07 16:08:19 +0200

    Chip Input

--- app/src/main/java/com/kl3jvi/pomodroid/view/activities/AddUpdateToDoList.kt
+++ app/src/main/java/com/kl3jvi/pomodroid/view/activities/AddUpdateToDoList.kt
@@ -1,12 +1,15 @@
 package com.kl3jvi.pomodroid.view.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.hootsuite.nachos.terminator.ChipTerminatorHandler
 import com.kl3jvi.pomodroid.R
 import com.kl3jvi.pomodroid.databinding.ActivityAddUpdateToDoListBinding
 
-class AddUpdateToDoList : AppCompatActivity() {
-    private lateinit var mBinding : ActivityAddUpdateToDoListBinding
+class AddUpdateToDoList : AppCompatActivity(), View.OnClickListener {
+    private lateinit var mBinding: ActivityAddUpdateToDoListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +17,24 @@ class AddUpdateToDoList : AppCompatActivity() {
         setContentView(mBinding.root)
         supportActionBar?.title = getString(R.string.add_task)
 
+        mBinding.btSave.setOnClickListener(this)
+        mBinding.btCancel.setOnClickListener(this)
 
+        mBinding.categoryChip.addChipTerminator('\n', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_ALL);
+        mBinding.categoryChip.addChipTerminator(' ', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_ALL);
+
+
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.bt_save -> {
+                    val title = mBinding.edTitle.text.toString().trim { it <= ' ' }
+                    Log.e("Titulli",title)
+                }
+            }
+
+        }
     }
 }
