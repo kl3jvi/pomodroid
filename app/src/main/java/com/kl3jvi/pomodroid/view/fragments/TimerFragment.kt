d8a93873155d433e65db3c91a3e6811a9ee0ba1a
kl3jvi <klejvisiper@gmail.com>
2021-08-17 18:20:00 +0200

    Animated Circular Progress and countdown timer fix

--- app/src/main/java/com/kl3jvi/pomodroid/view/fragments/TimerFragment.kt
+++ app/src/main/java/com/kl3jvi/pomodroid/view/fragments/TimerFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceManager
 import com.kl3jvi.pomodroid.databinding.FragmentTimerBinding
+import me.zhanghai.android.materialplaypausedrawable.MaterialPlayPauseDrawable
+
 
 class TimerFragment : Fragment() {
 
@@ -25,13 +28,54 @@ class TimerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mBinding!!.time.base = SystemClock.elapsedRealtime() + 1_500_000
-        mBinding!!.time.isCountDown = true
-        mBinding!!.time.start()
+        val chronometer = mBinding!!.time
+        chronometer.base = SystemClock.elapsedRealtime() + transformTime("focus")
+        chronometer.isCountDown = true
+
+
+
+        mBinding!!.playPause.setOnClickListener {
+            startStop()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         mBinding = null
+    }
+
+    private fun transformTime(key: String): Int {
+        val sharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(requireContext())
+        return sharedPreferences.getInt(key, 0) * 60000
+    }
+
+    private fun startStop() {
+
+
+        val newState =
+            if (mBinding!!.playPause.state === MaterialPlayPauseDrawable.State.Play)
+                MaterialPlayPauseDrawable.State.Pause else MaterialPlayPauseDrawable.State.Play
+
+        mBinding!!.playPause.state = newState
+
+
+        when (newState) {
+            MaterialPlayPauseDrawable.State.Play -> {
+                mBinding!!.time.stop()
+            }
+            MaterialPlayPauseDrawable.State.Pause -> {
+                mBinding!!.time.start()
+                val max = transformTime("focus") / 1000F
+                mBinding!!.time.setOnChronometerTickListener {
+                    val elapsedMillis: Long = SystemClock.elapsedRealtime() - it.base
+                    mBinding!!.circularProgressBar.progressMax = max
+                    mBinding!!.circularProgressBar.setProgressWithAnimation(
+                        ((max - elapsedMillis) / 1000),
+                        1000
+                    )
+                }
+            }
+        }
     }
 }
